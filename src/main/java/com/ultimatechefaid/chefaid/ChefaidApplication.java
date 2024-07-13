ae1998febfcf8dc9912f153fd926cd5f62672bb3
nomadriding <fundora.a@gmail.com>
2024-07-13 12:06:13 -0400

    initial commit

---
+++ src/main/java/com/ultimatechefaid/chefaid/ChefaidApplication.java
@@ -0,0 +1,13 @@
+package com.ultimatechefaid.chefaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultimatechefaid/chefaid/ChefaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultimatechefaid.chefaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
